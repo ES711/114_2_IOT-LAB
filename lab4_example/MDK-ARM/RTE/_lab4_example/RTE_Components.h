51a7b70a9f085c5272ee51bf42deab7a9cff6829
ES711 <multitaskyuntech@gmail.com>
2026-01-22 13:53:09 +0800

    lab4

---
+++ lab4_example/MDK-ARM/RTE/_lab4_example/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'lab4_example' 
+ * Target:  'lab4_example' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
