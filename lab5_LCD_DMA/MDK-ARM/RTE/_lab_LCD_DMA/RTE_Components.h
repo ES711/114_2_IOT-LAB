95d66b39c7df848fea4bf5544cfe63111ce39911
ES711 <multitaskyuntech@gmail.com>
2026-02-02 16:23:06 +0800

    lab5

---
+++ lab5_LCD_DMA/MDK-ARM/RTE/_lab_LCD_DMA/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'lab_LCD_DMA' 
+ * Target:  'lab_LCD_DMA' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
