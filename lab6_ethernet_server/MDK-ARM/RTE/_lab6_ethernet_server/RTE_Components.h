32aab5031aaba39d39ae230deb2473815d11a2b2
ES711 <multitaskyuntech@gmail.com>
2026-04-13 16:23:45 +0800

    Lab6_server

---
+++ lab6_ethernet_server/MDK-ARM/RTE/_lab6_ethernet_server/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'lab6_ethernet_server' 
+ * Target:  'lab6_ethernet_server' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
